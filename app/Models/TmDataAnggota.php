917265a6150d135054e408124cb9dcce41415acf
Muhammad ilham <ilhamgallagher633@gmail.com>
2023-06-08 01:41:05 +0700

    add tm data anggota migration seed

--- app/Models/TmDataAnggota.php
+++ app/Models/TmDataAnggota.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class TmDataPendaftar extends Model
+class TmDataAnggota extends Model
 {
-    protected $table = 'tm_data_pendaftar';
+    protected $table = 'tm_data_anggota';
 
     protected $fillable = ['name','email','nim','no_telepon','jenis_kelamin','tanggal_lahir','ukuran_baju','alamat','image_url','status','created_at','updated_at'];
 
