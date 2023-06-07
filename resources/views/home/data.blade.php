20c713ec677a437e9a8758219b6a0b53e55a0a98
Muhammad ilham <ilhamgallagher633@gmail.com>
2023-06-07 15:26:09 +0700

    add pdf view

--- resources/views/home/data.blade.php
+++ resources/views/home/data.blade.php
@@ -48,78 +48,64 @@
     @include('layouts.header')
 
     <main>
-    <!-- Trending Area Start -->
-    <div class="trending-area fix">
-        <div class="container">
+        <!-- Trending Area Start -->
+        <div class="trending-area fix">
+          <div class="container">
             <div class="trending-main">
-                <!-- Trending Tittle -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="trending-tittle">
-                            
-                            <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
-                            
-                        </div>
-                    </div>
+              <!-- Trending Tittle -->
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="trending-tittle">
+                    <!-- <p>Rem ipsum dolor sit amet, consectetur adipisicing elit.</p> -->
+                  </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-8">
-                        <!-- Trending Top -->
-                        <div class="trending-top mb-30">
-                            
-                            <div class="trend-top-img">
-                                <img src="{{ $trending->image_url }}" alt="" width="300">
-                            </div>
-                            
+              </div>
+              <div class="row">
+                <div class="col-lg-8">
+                  <!-- Trending Top -->
+                  <!-- Trending Bottom -->
+                  <div class="trending-bottom mb-3 text-center"> <!-- Add "text-center" class here -->
+                    <div class="container">
+                      <div class="row mb-2 justify-content-center">
+                        <h2>Data Potensi Gugus Depan Ambacana <br> Ars University</h2>
+                      </div>
+                      <div class="row" id="pdf-viewer">
+                        <div class="col-lg-12 d-flex justify-content-center align-items-center">
+                          <canvas id="pdf-canvas"></canvas>
                         </div>
-                        <!-- Trending Bottom -->
-                        <div class="trending-bottom mb-3">
-                            <div class="container">
-                                <div class="row mb-3">
-                                    <h2>DATA POTENSI AMBACANA ARS UNIVERSITY</h2>
-                                </div>
-                                <div class="row">
-                                    <p>ini nantinya penjelasan logo.</p> <br>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                        
+                      </div>
                     </div>
-                    <!-- Riht content -->
-                    @include('layouts.right')
-                    
+                  </div>
                 </div>
+                <!-- Right content -->
+                @include('layouts.right')
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-    <!-- Trending Area End -->
-    <!--   Weekly-News start -->
-    @include('layouts.end-content')
-               
-    <!-- End Weekly-News -->
-   <!-- Whats New Start -->
-   
-    <!-- Whats New End -->
-    <!--   Weekly2-News start -->
-          
-    <!-- End Weekly-News -->
-    <!-- Start Youtube -->
-   
-    <!-- End Start youtube -->
-    <!--  Recent Articles start -->
-          
-    <!--Recent Articles End -->
-    <!--Start pagination -->
-
-    <!-- End pagination  -->
-    </main>
+        <!-- Trending Area End -->
+        <!-- Weekly-News start -->
+        @include('layouts.end-content')
+        <!-- End Weekly-News -->
+        <!-- Whats New Start -->
+        <!-- Whats New End -->
+        <!-- Weekly2-News start -->
+        <!-- End Weekly-News -->
+        <!-- Start Youtube -->
+        <!-- End Start youtube -->
+        <!-- Recent Articles start -->
+        <!-- Recent Articles End -->
+        <!-- Start pagination -->
+        <!-- End pagination -->
+      </main>
+      
     
    @include('layouts.footer2')
    
 	<!-- JS here -->
     {{ URL::asset('/build/css/bootstrap.min.css') }}
 		<!-- All JS Custom Plugins Link Here here -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.min.js"></script>
         <script src="{{ URL::asset('/assets/js/vendor/modernizr-3.5.0.min.js') }}"></script>
 		<!-- Jquery, Popper, Bootstrap -->
 		<script src="{{ URL::asset('/assets/js/vendor/jquery-1.12.4.min.js') }}"></script>
@@ -157,6 +143,39 @@
 		<!-- Jquery Plugins, main Jquery -->	
         <script src="{{ URL::asset('/assets/js/plugins.js') }}"></script>
         <script src="{{ URL::asset('/assets/js/main.js') }}"></script>
+        <script>
+            // Get the path to your PDF file
+            var pdfPath = "/assets/pdf/data.pdf";
+          
+            // PDF.js configuration options
+            var pdfjsLib = window['pdfjs-dist/build/pdf'];
+          
+            // Load the PDF document
+            pdfjsLib.getDocument(pdfPath).promise.then(function(pdf) {
+              // Get the first page of the PDF
+              return pdf.getPage(1);
+            }).then(function(page) {
+              var canvas = document.getElementById('pdf-canvas');
+              var context = canvas.getContext('2d');
+          
+              // Set the desired scale (e.g., 2 for double the size)
+              var scale = 1.5;
+          
+              // Get the viewport of the page at the desired scale
+              var viewport = page.getViewport({ scale: scale });
+          
+              // Set the canvas size to match the viewport
+              canvas.width = viewport.width;
+              canvas.height = viewport.height;
+          
+              // Render the page content on the canvas
+              var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+              };
+              page.render(renderContext);
+            });
+        </script>
         
     </body>
 </html>
